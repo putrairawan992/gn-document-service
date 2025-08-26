ccb732dfd6f41dbcac33a859cb49dca0583b8de6
Krisna <kciptaraksa@gmail.com>
2025-08-26 07:29:51 +0700

    change routing api

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\Api\DocumentController;
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('documents', [DocumentController::class, 'index']);
+    Route::get('documents/{id}', [DocumentController::class, 'show']);
+    Route::post('documents', [DocumentController::class, 'store']);
+    Route::post('documents/{id}', [DocumentController::class, 'update']);
+    Route::delete('documents/{id}', [DocumentController::class, 'destroy']);
 });
